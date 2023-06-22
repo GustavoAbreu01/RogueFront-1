f52834e23cd4d7889e9528c5a81b228956ef11af
Gustavo Guilherme de Abreu <110638920+Fennek01@users.noreply.github.com>
2023-06-22 14:38:32 -0300

    ATT

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,21 +2,30 @@ import React from 'react'
 import './style.css'
 import SubHeader from '../SubHeader/SubHeader'
 import logo from "../../assets/img/logo-weg.png"
+import { Link } from 'react-router-dom'
 
 
 function Header() {
   return (
-    <><div class="ui menu header">
-      <div class="item logo">
-        <img src={logo} class="tiny ui fluid image"></img>
-      </div>
-      <div class="item fluid search">
-        <div class="ui icon input">
-          <input type="text" placeholder="Search..."/>
+    <>
+      <div className="ui grid header">
+        <div className="three wide column">
+          <img src={logo} className='logo image' />
+        </div>
+        <div className="eight wide column">
+          <div class="ui icon input">
+            <input type="text" placeholder="Search..." className='search input' />
             <i class="search icon"></i>
+          </div>
+        </div>
+        <div className="three wide column">
+          <Link to={'/register'}><p className='text header'>Crie sua conta / Login</p></Link>
+        </div>
+        <div className="two wide column">
+          <i className="shopping cart icon"></i>
         </div>
       </div>
-    </div><SubHeader /></>
+      <SubHeader /></>
   )
 }
 
--- src/Components/SubHeader/SubHeader.js
+++ src/Components/SubHeader/SubHeader.js
@@ -3,14 +3,8 @@ import './style.css'
 
 function SubHeader() {
     return (
-        <div class="ui menu sub">
-            <div class="item">
-                <div class="ui button icon">
-                    <i class="list icon"></i>
-                </div>
-            </div>
-            <div class="right item">
-            </div>
+        <div >
+
         </div>
     )
 }
