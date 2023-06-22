ca8eced4d4573e2ee57858e7d8c121ef9c04d9ec
Vitor Manoel Alves de Andrade <vitorgml21@gmail.com>
2023-06-22 15:56:27 -0300

    att

---
+++ src/Components/CompareCard/CompareCard.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import "./style.css"
+import { MdCompareArrows, MdDelete } from 'react-icons/md'
+
+function CompareCard() {
+
+    let productsCompared = JSON.parse(localStorage.getItem("productsCompared"))
+
+    function verify(){
+        if(productsCompared.length === 3){
+          return true
+        }else{
+          return false
+        }
+    }
+
+    const deleteProduct = (index) => {
+        const updatedProductsCompared = productsCompared.filter((_, i) => i !== index);
+        localStorage.setItem('productsCompared', JSON.stringify(updatedProductsCompared));
+        productsCompared = updatedProductsCompared;
+        window.location.reload()
+      };
+
+    return (
+        <div className='productsCompared'>
+            {productsCompared.map((productCompared, index) => (
+                <div className='productComparedCard'>
+                    <div className="productCompared" key={index}>
+                        <div className='deleteButton'>
+                            {verify() &&<MdDelete size={'2rem'}  onClick={() => deleteProduct(index)}/>}   
+                        </div>
+                        <div>
+                            <img src={productCompared.imageUrl} alt="" className='productImage' />
+                        </div>
+                        <div>
+                            <h1>{productCompared.name}</h1>
+                        </div>
+                        <div>
+                            <h2>{productCompared.description1}</h2>
+                            <h2>{productCompared.description2}</h2>
+                            <h2>{productCompared.description3}</h2>
+                            <h2>{productCompared.description4}</h2>
+                        </div>
+                    </div>
+                </div>
+            ))}
+            <div className='compareArrow'>
+                <MdCompareArrows size={'3rem'} />
+            </div>
+            {verify() &&<div className='compareArrow2'>
+                <MdCompareArrows size={'3rem'} />
+            </div>}
+        </div>
+        
+    )
+}
+
+export default CompareCard
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,30 @@
 import React from 'react'
+import logo from "../../assets/img/logo-weg.png"
+import salvo from "../../assets/img/salvo.png"
 
 function Home() {
+
+  const products = [
+    {
+        name: 'Logo',
+        imageUrl: logo,
+        description1: 'logo1',
+        description2: 'logo2',
+        description3: 'logo3',
+        description4: 'logo4'
+    },
+    {
+        name: 'Salvo',
+        imageUrl: salvo,
+        description1: 'salvo1',
+        description2: 'salvo2',
+        description3: 'salvo3',
+        description4: 'salvo4'
+    }
+];
+
+localStorage.setItem("productsCompared", JSON.stringify(products))
+
   return (
     <div>Home</div>
   )
