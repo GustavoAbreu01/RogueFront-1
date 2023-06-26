8ba1a63ea5a00929ce2e5032ef2f6ed6fe23c021
Gustavo Guilherme de Abreu <110638920+Fennek01@users.noreply.github.com>
2023-06-26 16:55:53 -0300

    ATT

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,9 +2,11 @@ import React from 'react'
 import Footer from '../../Components/Footer/Footer'
 import MenuModal from '../../Components/MenuModal/MenuModal'
 import Header from '../../Components/Header/Header'
+import Carousell from '../../Components/carousel/carousel'
 function Home() {
   return (
     <><Header />
+    <Carousell></Carousell>
     
     
     <Footer /></>
