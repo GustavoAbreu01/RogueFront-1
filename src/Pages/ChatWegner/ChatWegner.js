e1d0278fa38bc7c9416382d5a77ad12ab2d853e4
Vitor Manoel Alves de Andrade <vitorgml21@gmail.com>
2023-06-26 15:20:07 -0300

    att

--- src/Pages/ChatWegner/ChatWegner.js
+++ src/Pages/ChatWegner/ChatWegner.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './style.css'
 import wegnerAcordado from '../../assets/img/weggnerAcordado.PNG'
 import { IoIosArrowBack } from 'react-icons/io'
+import { VscSend } from 'react-icons/vsc'
 
 function ChatWegner() {
     return (
@@ -18,10 +19,21 @@ function ChatWegner() {
                     </div>
                 </div>
                 <div className='chatMessages'>
-                    mensagens
+                    <div>
+                        <div>
+                            Mensagens
+                        </div>
+                    </div>
                 </div>
                 <div className='chatOptions'>
-                    mandarMSG
+                    <div className='options'>
+                        <div>
+                            <input type='text' className='inputMessages'/>
+                        </div>
+                        <div >
+                            <VscSend size={'2.5rem'} className='sendButton'/>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
