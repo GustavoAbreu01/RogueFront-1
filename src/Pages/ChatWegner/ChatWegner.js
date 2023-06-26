e7f07fc14f9eb13ec2dc0bc0a7cd6f314fe813e2
Vitor Manoel Alves de Andrade <vitorgml21@gmail.com>
2023-06-26 14:49:07 -0300

    att

--- src/Pages/ChatWegner/ChatWegner.js
+++ src/Pages/ChatWegner/ChatWegner.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import './style.css'
 import wegnerAcordado from '../../assets/img/weggnerAcordado.PNG'
+import { IoIosArrowBack } from 'react-icons/io'
 
 function ChatWegner() {
     return (
         <div className='pagChat'>
+            <div className='wegner'>
+                <img src={wegnerAcordado} alt='' width={200}/>
+            </div>
             <div className='chat'>
                 <div className='chatHeader'>
-                    opções
+                    <div>
+                        <div className='backArrow'>
+                            <IoIosArrowBack size={'2.5rem'}/>
+                        </div>
+                    </div>
                 </div>
                 <div className='chatMessages'>
                     mensagens
