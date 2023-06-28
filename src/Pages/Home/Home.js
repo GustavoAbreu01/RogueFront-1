125c1cf6c3b2f19013dab1fd43110b5c7f30ba8e
Gustavo Guilherme de Abreu <110638920+Fennek01@users.noreply.github.com>
2023-06-28 15:53:18 -0300

    ATT

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,8 +2,8 @@ import React from 'react'
 import questIcon from "../../assets/img/questionario.png"
 import compare from '../../assets/img/compare.png'
 import weggner from '../../assets/img/WagnerDormindo.png'
-import Header from '../../Components/Header/Header'
 import Footer from '../../Components/Footer/Footer'
+import Header from '../../Components/Header/Header'
 import Carousell from '../../Components/carousel/Carousel'
 import Carousel from "react-multi-carousel";
 import { BiLineChart } from 'react-icons/bi'
@@ -12,6 +12,17 @@ import './style.css'
 import { Link } from 'react-router-dom'
 
 function Home() {
+
+  const login = localStorage.getItem("verifyLogin")
+
+  const verifyLogin = () => {
+    if (login === 'yes') {
+      return true
+    } else {
+      return false
+    }
+  }
+
 
   const responsive = {
     desktop: {
@@ -31,17 +42,6 @@ function Home() {
     }
   };
 
-
-  const login = localStorage.getItem("verifyLogin")
-
-  const verifyLogin = () => {
-    if (login == 'yes') {
-      return true
-    } else {
-      return false
-    }
-  }
-
   return (
     <><Header />
       <div className='boxCarrossel'>
@@ -49,26 +49,26 @@ function Home() {
       </div>
       <div className="dividerCarrossel"></div>
       <div className="optionPages">
-        <Link>
+        <Link to='/quiz' style={{ textDecoration: 'none' }}>
           <div className="optionQuest">
             <div className="questIcon">
               <img src={questIcon} alt="" width={75} />
               <div>
-                <h4>Descubra qual é<br />
+                <h4 className='titleOpc'>Descubra qual é<br />
                   o produto para você!</h4>
-                <p>Faça o nosso questionário</p>
+                <p className='descOpc'>Faça o nosso questionário</p>
               </div>
             </div>
           </div>
         </Link>
         <div className='divider'></div>
-        <Link>
+        <Link to='/compare' style={{ textDecoration: 'none' }}>
           <div className="optionQuest">
             <div className="questIcon">
               <img src={compare} alt="" width={150} />
               <div>
-                <h3>Compare</h3>
-                <p>Veja qual se encaixa melhor<br />
+                <h4 className='titleOpc'>Compare produtos</h4>
+                <p className='descOpc'>Veja qual se encaixa melhor<br />
                   com a sua necessidade</p>
               </div>
             </div>
@@ -76,38 +76,37 @@ function Home() {
         </Link>
         <div className='divider'></div>
         <Link to='*' style={{ textDecoration: 'none' }}>
-          <div className="optionQuest">
+          <div className="optionWeggner">
             <div className="questIcon">
               <img src={weggner} alt="" width={125} />
               <div>
-                <h3>Conheça o nosso<br />
-                  Assistente Virtual!</h3>
-                <p>Navegar ficou ainda mais fácil</p>
+                <h4 className='titleOpc'>Conheça o nosso<br />
+                  Assistente Virtual!</h4>
+                <p className='descOpc'>Navegar ficou ainda mais fácil</p>
               </div>
             </div>
           </div>
         </Link>
+      </div>
+      <div className='boxAccessed'>
+        <div className='boxTitleAccessed'>
+          <BiLineChart color='var(--blue-primary)' size={30} />
+          <h3 className='titleAccessed' >Produtos mais acessados</h3>
         </div>
-        <div className='boxAccessed'>
-          <div className='boxTitleAccessed'>
-            <BiLineChart color='var(--blue-primary)' size={30} />
-            <h3 className='titleAccessed' >Produtos mais acessados</h3>
-          </div>
-          <div className='sliderProductAccessed'>
-            <Carousel
-              swipeable={false}
-              draggable={false}
-              responsive={responsive}>
-              <div>Item 1</div>
-              <div>Item 2</div>
-              <div>Item 3</div>
-              <div>Item 4</div>
-            </Carousel>
-          </div>
+        <div className='sliderProductAccessed'>
+          <Carousel
+            swipeable={false}
+            draggable={false}
+            responsive={responsive}>
+            <div>Item 1</div>
+            <div>Item 2</div>
+            <div>Item 3</div>
+            <div>Item 4</div>
+          </Carousel>
         </div>
-        <Footer />
-        </>
-      )
+      </div>
+      <Footer /></>
+  )
 }
 
-      export default Home
+export default Home
