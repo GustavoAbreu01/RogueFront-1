bfe85f8b0e648448d6e98fc354a0a699b1bf9f91
Gustavo Guilherme de Abreu <110638920+Fennek01@users.noreply.github.com>
2023-06-28 16:03:47 -0300

    ATT

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -6,19 +6,7 @@ function Product() {
   const numero = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   return (
     <>
-      {/* <div className="boxProduct">
-        <div className="containerProduct">
-          <div className="imageProduct">
-            <img className="ImageMotor"src={motor} />
-          </div>
-          <div className="contentProduct">
-            <div className="titleProduct">Motor</div>
-            <div className="piceTagProduct">
-              <a className='priceProduct'>R$23,00</a>
-            </div>
-          </div>
-        </div>
-      </div> */}
+      
       <div id="produtos" className='produtos'>
         {numero.map((i) => (
 
