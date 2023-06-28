8e8bb5427ad1aeb92b05dc3c812fa33c0a69a3c6
Gustavo Guilherme de Abreu <110638920+Fennek01@users.noreply.github.com>
2023-06-28 13:40:45 -0300

    ATT

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -4,33 +4,46 @@ import compare from '../../assets/img/compare.png'
 import weggner from '../../assets/img/WagnerDormindo.png'
 import Footer from '../../Components/Footer/Footer'
 import Header from '../../Components/Header/Header'
-import Carousell from '../../Components/carousel/carousel'
+import Carousell from '../../Components/carousel/Carousel'
 import './style.css'
 
 function Home() {
   return (
     <><Header />
+    <div className='boxCarrossel'></div>
       <Carousell></Carousell>
       <div className="dividerCarrossel"></div>
       <div className="optionPages">
         <div className="optionQuest">
           <div className="questIcon">
             <img src={questIcon} alt="" width={75} />
-            <p>aaaaaaaaaaaaa</p>
+            <div>
+              <h4>Descubra qual é<br />
+                o produto para você!</h4>
+              <p>Faça o nosso questionário</p>
+            </div>
           </div>
         </div>
         <div className='divider'></div>
         <div className="optionQuest">
           <div className="questIcon">
             <img src={compare} alt="" width={150} />
-            <p>aaaaaaaaaaaaa</p>
+            <div>
+              <h3>Compare</h3>
+              <p>Veja qual se encaixa melhor<br />
+                com a sua necessidade</p>
+            </div>
           </div>
         </div>
         <div className='divider'></div>
         <div className="optionQuest">
           <div className="questIcon">
             <img src={weggner} alt="" width={125} />
-            <p>aaaaaaaaaaaaa</p>
+            <div>
+              <h3>Conheça o nosso<br/>
+                Assistente Virtual!</h3>
+              <p>Navegar ficou ainda mais fácil</p>
+            </div>
           </div>
         </div>
       </div>
