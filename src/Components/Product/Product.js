2c63c8f873ea4868ebf2ddb6f800d7c40018054d
Bruneb4s <105462955+Brunebas@users.noreply.github.com>
2023-06-28 15:37:05 -0300

    ATT

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import motor from '../../assets/img/motor.png'
+import motors from '../../assets/img/motor.png'
+import save from '../../assets/img/salvo.png'
 import './style.css'
 function Product() {
+  const numero = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   return (
     <>
-      <div className="boxProduct">
+      {/* <div className="boxProduct">
         <div className="containerProduct">
           <div className="imageProduct">
             <img className="ImageMotor"src={motor} />
@@ -16,6 +18,27 @@ function Product() {
             </div>
           </div>
         </div>
+      </div> */}
+      <div id="produtos" className='produtos'>
+        {numero.map((i) => (
+
+          <div class="ui segment itens" id="item" key={i}>
+            <div class="itemContent">
+              <button className="salvos" href="">
+                <img src={save} className="salvosimg" width="40" height="40" id="salvos" />
+              </button>
+              <div id="imgItens">
+                <img src={motors} width="80%" height="" className='motor' />
+              </div>
+              <div class="itemInfo">
+
+                <h2 id="itemNome">W12 Monofásico</h2>
+                <h4 id="descricao">Desenvolvida para oferecer versatilidade e eficiência.</h4>
+                <h2 id="itemPreco">R$ 495<sup> 99</sup></h2>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </>
   )
