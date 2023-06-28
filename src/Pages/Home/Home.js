3b8d6be5d2d93fb0e5efcf1368cb5d4d6f243069
Gustavo Guilherme de Abreu <110638920+Fennek01@users.noreply.github.com>
2023-06-28 14:28:02 -0300

    ATT

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -5,47 +5,62 @@ import weggner from '../../assets/img/WagnerDormindo.png'
 import Footer from '../../Components/Footer/Footer'
 import Header from '../../Components/Header/Header'
 import Carousell from '../../Components/carousel/Carousel'
+import { BiLineChart } from 'react-icons/bi'
+
 import './style.css'
+import { Link } from 'react-router-dom'
 
 function Home() {
   return (
     <><Header />
-    <div className='boxCarrossel'>
-      <Carousell></Carousell>
+      <div className='boxCarrossel'>
+        <Carousell></Carousell>
       </div>
       <div className="dividerCarrossel"></div>
       <div className="optionPages">
+      <Link to='/quiz' style={{ textDecoration: 'none' }}>
         <div className="optionQuest">
           <div className="questIcon">
             <img src={questIcon} alt="" width={75} />
             <div>
-              <h4>Descubra qual é<br />
+              <h4 className='titleOpc'>Descubra qual é<br />
                 o produto para você!</h4>
-              <p>Faça o nosso questionário</p>
+              <p className='descOpc'>Faça o nosso questionário</p>
             </div>
           </div>
         </div>
+        </Link>
         <div className='divider'></div>
+        <Link to='/compare' style={{ textDecoration: 'none' }}>
         <div className="optionQuest">
           <div className="questIcon">
             <img src={compare} alt="" width={150} />
             <div>
-              <h3>Compare</h3>
-              <p>Veja qual se encaixa melhor<br />
+              <h4 className='titleOpc'>Compare produtos</h4>
+              <p className='descOpc'>Veja qual se encaixa melhor<br />
                 com a sua necessidade</p>
             </div>
           </div>
         </div>
+        </Link>
         <div className='divider'></div>
-        <div className="optionQuest">
+        <Link to='*' style={{ textDecoration: 'none' }}>
+        <div className="optionWeggner">
           <div className="questIcon">
             <img src={weggner} alt="" width={125} />
             <div>
-              <h3>Conheça o nosso<br/>
-                Assistente Virtual!</h3>
-              <p>Navegar ficou ainda mais fácil</p>
+              <h4 className='titleOpc'>Conheça o nosso<br />
+                Assistente Virtual!</h4>
+              <p className='descOpc'>Navegar ficou ainda mais fácil</p>
             </div>
           </div>
+        </div>
+        </Link>
+      </div>
+      <div className='boxAccessed'>
+        <div className='boxTitleAccessed'>
+          <BiLineChart color='var(--blue-primary)' size={30} />
+          <h3 className='titleAccessed' >Produtos mais acessados</h3>
         </div>
       </div>
       <Footer /></>
