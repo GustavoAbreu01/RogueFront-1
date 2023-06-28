82cb4a0fde16d97d14896615ce374e02a1d9a1fd
Gustavo Guilherme de Abreu <110638920+Fennek01@users.noreply.github.com>
2023-06-28 13:20:16 -0300

    ATT

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import questIcon from "../../assets/img/questionario.png"
+import compare from '../../assets/img/compare.png'
+import weggner from '../../assets/img/WagnerDormindo.png'
 import Footer from '../../Components/Footer/Footer'
 import Header from '../../Components/Header/Header'
 import Carousell from '../../Components/carousel/carousel'
 import './style.css'
-import { Divider } from 'semantic-ui-react'
 
 function Home() {
   return (
@@ -14,20 +15,21 @@ function Home() {
       <div className="optionPages">
         <div className="optionQuest">
           <div className="questIcon">
-            <img src={questIcon} alt="" />
-            <p>aaaaaaaaaaaaa</p>
-          </div>
-          <div className=''></div>
-        </div>
-        <div className="optionQuest">
-          <div className="questIcon">
-            <img src={questIcon} alt="" />
+            <img src={questIcon} alt="" width={75} />
             <p>aaaaaaaaaaaaa</p>
           </div>
         </div>
+        <div className='divider'></div>
         <div className="optionQuest">
           <div className="questIcon">
-            <img src={questIcon} alt="" />
+            <img src={compare} alt="" width={150} />
+            <p>aaaaaaaaaaaaa</p>
+          </div>
+        </div>
+        <div className='divider'></div>
+        <div className="optionQuest">
+          <div className="questIcon">
+            <img src={weggner} alt="" width={125} />
             <p>aaaaaaaaaaaaa</p>
           </div>
         </div>
