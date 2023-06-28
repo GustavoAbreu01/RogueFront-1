ca983bafe57b7b6c585111c03f3015538c73871c
Gustavo Guilherme de Abreu <110638920+Fennek01@users.noreply.github.com>
2023-06-28 15:54:15 -0300

    ATT

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -11,33 +11,33 @@ import MenuModal from '../MenuModal/MenuModal'
 function Header() {
   return (
     <><header>
-      <div className="img">
+      <div class="img">
         <Link to={"/"}><img src={logo} alt="" /></Link>
       </div>
       <div>
         <SearchBar />
       </div>
-      <div className="opc">
-        <div className="opcDeEntrada">
-          <Link className="account" to="/register">Crie sua conta</Link>/<Link className="login" to="/register">Login</Link>
+      <div class="opc">
+        <div class="opcDeEntrada">
+          <Link class="account" to="/register">Crie sua conta</Link>/<Link class="login" to="/register">Login</Link>
         </div>
-        <Link className="saves" to="/cart"><FaShoppingCart className="carrinho" /></Link>
+        <Link class="saves" to="/cart"><FaShoppingCart class="carrinho" /></Link>
       </div>
-    </header><div className="navBar">
+    </header><div class="navBar">
         <div id="menu">
           <MenuModal />
         </div>
-        <div className="divisores"></div>
-        <Link to="/categoria">Motores</Link>
-        <div className="divisores"></div>
-        <Link to="/carrinho">Tintas e Vernizes</Link>
-        <div className="divisores"></div>
-        <Link href="#">Automação</Link>
-        <div className="divisores"></div>
-        <Link href="#">Paineis Eletricos</Link>
-        <div className="divisores"></div>
-        <Link href="#">Segurança</Link>
-        <div className="divisores"></div>
+        <div class="divisores"></div>
+        <Link routerLink="/categoria">Motores</Link>
+        <div class="divisores"></div>
+        <Link routerLink="/carrinho">Tintas e Vernizes</Link>
+        <div class="divisores"></div>
+        <Link href="">Automação</Link>
+        <div class="divisores"></div>
+        <Link href="">Paineis Eletricos</Link>
+        <div class="divisores"></div>
+        <Link href="">Segurança</Link>
+        <div class="divisores"></div>
       </div></>
   )
 }
