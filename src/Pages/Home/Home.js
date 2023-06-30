1377cf1fa6466d39791e75eb9140ac673214ba40
Gustavo Guilherme de Abreu <110638920+Fennek01@users.noreply.github.com>
2023-06-30 13:17:16 -0300

    ATT

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -45,72 +45,62 @@ function Home() {
 
   return (
     <div>
-      <><Header />
-      <div className='boxCarrossel'>
-        <Carousell></Carousell>
-      </div>
-      <div className="dividerCarrossel"></div>
-      <WeggnerModal/>
-    </div>
-      <div className="optionPages">
-        <Link to='/quiz' style={{ textDecoration: 'none' }}>
-          <div className="optionQuest">
-            <div className="questIcon">
-              <img src={questIcon} alt="" width={75} />
-              <div>
-                <h4 className='titleOpc'>Descubra qual é<br />
-                  o produto para você!</h4>
-                <p className='descOpc'>Faça o nosso questionário</p>
-              </div>
-            </div>
-          </div>
-        </Link>
-        <div className='dividerIcons'></div>
-        <Link to='/compare' style={{ textDecoration: 'none' }}>
-          <div className="optionQuest">
-            <div className="questIcon">
-              <img src={compare} alt="" width={150} />
-              <div>
-                <h4 className='titleOpc'>Compare produtos</h4>
-                <p className='descOpc'>Veja qual se encaixa melhor<br />
-                  com a sua necessidade</p>
-              </div>
-            </div>
-          </div>
-        </Link>
-        <div className='dividerIcons'></div>
-        <Link to='*' style={{ textDecoration: 'none' }}>
-          <div className="optionWeggner">
-            <div className="questIcon">
-              <img src={weggner} alt="" width={125} />
-              <div>
-                <h4 className='titleOpc'>Conheça o nosso<br />
-                  Assistente Virtual!</h4>
-                <p className='descOpc'>Navegar ficou ainda mais fácil</p>
-              </div>
-            </div>
-          </div>
-        </Link>
-      </div>
-      <div className='boxAccessed'>
-        <div className='boxTitleAccessed'>
-          <BiLineChart color='var(--blue-primary)' size={30} />
-          <h3 className='titleAccessed' >Produtos mais acessados</h3>
+      <><Header /><WeggnerModal />
+        <div className='boxCarrossel'>
+          <Carousell></Carousell>
         </div>
-        <div className='sliderProductAccessed'>
-          <Carousel
-            swipeable={false}
-            draggable={false}
-            responsive={responsive}>
-            <div>Item 1</div>
-            <div>Item 2</div>
-            <div>Item 3</div>
-            <div>Item 4</div>
-          </Carousel>
+        <div className="dividerCarrossel"></div>
+        <div className="optionPages">
+          <Link to='/quiz' style={{ textDecoration: 'none' }}>
+            <div className="optionQuest">
+              <div className="questIcon">
+                <img src={questIcon} alt="" width={75} />
+                <div>
+                  <h4 className='titleOpc'>Descubra qual é<br />
+                    o produto para você!</h4>
+                  <p className='descOpc'>Faça o nosso questionário</p>
+                </div>
+              </div>
+            </div>
+          </Link>
+          <div className='dividerIcons'></div>
+          <Link to='/compare' style={{ textDecoration: 'none' }}>
+            <div className="optionQuest">
+              <div className="questIcon">
+                <img src={compare} alt="" width={150} />
+                <div>
+                  <h4 className='titleOpc'>Compare produtos</h4>
+                  <p className='descOpc'>Veja qual se encaixa melhor<br />
+                    com a sua necessidade</p>
+                </div>
+              </div>
+            </div>
+          </Link>
+          <div className='dividerIcons'></div>
+          <Link to='*' style={{ textDecoration: 'none' }}>
+            <div className="optionWeggner">
+              <div className="questIcon">
+                <img src={weggner} alt="" width={125} />
+                <div>
+                  <h4 className='titleOpc'>Conheça o nosso<br />
+                    Assistente Virtual!</h4>
+                  <p className='descOpc'>Navegar ficou ainda mais fácil</p>
+                </div>
+              </div>
+            </div>
+          </Link>
         </div>
-      </div>
-
+        <div className='boxAccessed'>
+          <div className='boxTitleAccessed'>
+            <BiLineChart color='var(--blue-primary)' size={30} />
+            <h3 className='titleAccessed' >Produtos mais acessados</h3>
+          </div>
+          <div className='sliderProductAccessed'>
+          </div>
+        </div>
       <Footer /></>
+      </div>
+      
   )
 }
 
