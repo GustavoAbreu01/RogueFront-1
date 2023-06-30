439b469a9a6e6c073a2dd512b4557585b4b6265c
Gustavo Guilherme de Abreu <110638920+Fennek01@users.noreply.github.com>
2023-06-30 16:55:08 -0300

    ATT

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,32 +1,9 @@
 import React from 'react'
-import motors from '../../assets/img/motor.png'
-import save from '../../assets/img/salvo.png'
-import './style.css'
-function Product() {
-  const numero = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function ProfileModal() {
   return (
-    <>
-
-      <div id="produtos" className='produtos'>
-        {numero.map((i) => (
-
-            <div className="boxProduct">
-              <div className="containerProduct">
-                <div className="imageProduct">
-                  <img className="ImageMotor" src={motors} />
-                </div>
-                <div className="contentProduct">
-                  <div className="titleProduct">Motor</div>
-                  <div className="piceTagProduct">
-                    <a className='priceProduct'>R$23,00</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-        ))}
-      </div>
-    </>
+    <div>Product</div>
   )
 }
 
-export default Product
+export default ProfileModal
