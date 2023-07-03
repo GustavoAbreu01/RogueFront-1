bb2d34a8eb3e317ccff338571105ae24fd3ffecc
mary <105462946+mariazinhagameplys@users.noreply.github.com>
2023-07-03 15:58:17 -0300

    att

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import './NotFound.css'
-import SmallProductCard from '../../Components/SmallProductCard/SmallProductCard'
+
 
 function NotFound() {
   return (
     <div>
-    
-    <SmallProductCard/>
+  
 
     </div>
   )
