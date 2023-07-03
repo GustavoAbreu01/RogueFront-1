0b722434d0b64d012f67f3702fe02235fa8d2426
mary <105462946+mariazinhagameplys@users.noreply.github.com>
2023-07-03 16:55:31 -0300

    att socorro

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import './NotFound.css'
+import SmallProductHorizontal from "../../Components/SmallProductHorizontal/SmallProductHorizontal"
 
 
 function NotFound() {
   return (
     <div>
-  
-
+      <SmallProductHorizontal />
     </div>
   )
 }
