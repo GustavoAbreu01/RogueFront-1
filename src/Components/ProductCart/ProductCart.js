b178291cb4794710e46c32ad6c415bfb16fcc187
Bruneb4s <105462955+Brunebas@users.noreply.github.com>
2023-07-04 16:57:24 -0300

    ATT

--- src/Components/ProductCart/ProductCart.js
+++ src/Components/ProductCart/ProductCart.js
@@ -39,29 +39,29 @@ function ProductCart() {
 
   return (
     <>
-      <div className="row itens Horizontal">
-        <div className="column itens Horizontal">
-          <div className="itemContent Horizontal" style={{ color: 'black' }}>
-            <div id="imgItens Horizontal">
+      <div className="row itens CardCart">
+        <div className="column itens CardCart">
+          <div className="itemContent CardCart" style={{ color: 'black' }}>
+            <div id="imgItens CardCart">
               <Link to="/product">
                 <img src={motors} width="125" height="" />
               </Link>
             </div>
-            <div className="itemInfo Horizontal">
+            <div className="itemInfo CardCart">
               <Link to="/product">
-                <h2 className="itemNome Horizontal">W12 Monofásico</h2>
-                <h2 id="itemPreco Horizontal" className='itemPreco Horizontal'>R$ 495<sup> 99</sup><sub className='subtext'>10x Sem juros</sub></h2>
-                <p className="itemComplement Horizontal">Motor monofásico de carcaça de chapa, para uso geral, desenvolvido para atender as mais variadas aplicações...</p>
+                <h2 className="itemNome CardCart">W12 Monofásico</h2>
+                <h2 id="itemPreco CardCart" className='itemPreco CardCart'>R$ 495<sup> 99</sup><sub className='subtext'>10x Sem juros</sub></h2>
+                <p className="itemComplement CardCart">Motor monofásico de carcaça de chapa, para uso geral, desenvolvido para atender as mais variadas aplicações...</p>
               </Link>
             </div>
-            <div className='buttons Horizontal'>
-              <button class="ui icon button Horizontal">
-                <i className="trash alternate icon Horizontal"></i>
+            <div className='buttons CardCart'>
+              <button class="ui icon button CardCart">
+                <i className="trash alternate icon CardCart"></i>
               </button>
-              <div className="ui small buttons Horizontal">
-                <button className="ui button active Horizontal">-</button>
+              <div className="ui small buttons CardCart">
+                <button className="ui button active plus">-</button>
                 <div className='or' data-text="10"></div>
-                <button className="ui button Horizontal">+</button>
+                <button className="ui button plus">+</button>
               </div>
             </div>
           </div>
@@ -70,8 +70,4 @@ function ProductCart() {
     </>
   );
 };
-
-
-
-
 export default ProductCart
