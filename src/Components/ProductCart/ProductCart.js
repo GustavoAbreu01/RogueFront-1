7778cdb1de4c72c45fdc3cdb84ba03ce0b7a894c
Bruneb4s <105462955+Brunebas@users.noreply.github.com>
2023-07-05 13:47:03 -0300

    ATT

--- src/Components/ProductCart/ProductCart.js
+++ src/Components/ProductCart/ProductCart.js
@@ -1,42 +1,10 @@
 import React from 'react'
 import './ProductCart.css'
 import { Link } from "react-router-dom";
-import { useState, useEffect } from "react";
-import Header from '../../Components/Header/Header'
-import Footer from '../../Components/Footer/Footer'
 import motors from "../../assets/img/motores.png"
 
 
 function ProductCart() {
-  const [verificacao, setVerificacao] = useState(false);
-
-
-  const [isOpen, setIsOpen] = useState(false);
-
-
-  const handleDropdownChange = (event, { value }) => {
-    console.log('Selected value:', value);
-  };
-
-
-  const menuOptions = [
-    { key: 'monofasico', value: 'monofasico', text: 'Monofásico' },
-    { key: 'trifasicoBT', value: 'trifasicoBT', text: 'Trifásico - Baixa Tensão' },
-    { key: 'trifasicoAT', value: 'trifasicoAT', text: 'Trifásico - Alta Tensão' },
-    { key: 'sincronos', value: 'sincronos', text: 'Síncronos' },
-    { key: 'aneis', value: 'aneis', text: 'Anéis' },
-  ];
-
-
-
-
-  const teste = () => {
-    console.log('aaaaa')
-  };
-
-
-
-
   return (
     <>
       <div className="row itens CardCart">
@@ -50,7 +18,7 @@ function ProductCart() {
             <div className="itemInfo CardCart">
               <Link to="/product">
                 <h2 className="itemNome CardCart">W12 Monofásico</h2>
-                <h2 id="itemPreco CardCart" className='itemPreco CardCart'>R$ 495<sup> 99</sup><sub className='subtext'>10x Sem juros</sub></h2>
+                <h2 id="itemPreco CardCart" className='itemPreco CardCart'>R$ 495<sup> 99</sup><sub className='subtext CardCart'>10x Sem juros</sub></h2>
                 <p className="itemComplement CardCart">Motor monofásico de carcaça de chapa, para uso geral, desenvolvido para atender as mais variadas aplicações...</p>
               </Link>
             </div>
@@ -59,9 +27,9 @@ function ProductCart() {
                 <i className="trash alternate icon CardCart"></i>
               </button>
               <div className="ui small buttons CardCart">
-                <button className="ui button active plus">-</button>
-                <div className='or' data-text="10"></div>
-                <button className="ui button plus">+</button>
+                <button className="ui button active CardCart">-</button>
+                <div className='or CardCart' data-text="10"></div>
+                <button className="ui button CardCart">+</button>
               </div>
             </div>
           </div>
