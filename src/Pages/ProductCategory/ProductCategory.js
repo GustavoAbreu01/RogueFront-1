38d12b7b8b1d5d768b96ef070fe3c72b1646892c
mary <105462946+mariazinhagameplys@users.noreply.github.com>
2023-07-05 14:13:35 -0300

    att:)

--- src/Pages/ProductCategory/ProductCategory.js
+++ src/Pages/ProductCategory/ProductCategory.js
@@ -4,22 +4,31 @@ import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import Header from '../../Components/Header/Header'
 import Footer from '../../Components/Footer/Footer'
-import { Dropdown } from 'semantic-ui-react';
 import iconMotor from "../../assets/img/iconeMotor.png"
 import motors from "../../assets/img/motores.png"
 import save from "../../assets/img/salvo.png"
-import HeaderLogin from '../../Components/HeaderLogin/HeaderLogin';
-import WeggnerModal from '../../Components/WeggnerModal/WeggnerModal';
+
+
+
 
 function ProductCategory() {
   const [verificacao, setVerificacao] = useState(false);
 
+
+
+
   const [isOpen, setIsOpen] = useState(false);
+
+
+
 
   const handleDropdownChange = (event, { value }) => {
     console.log('Selected value:', value);
     // Aqui você pode adicionar a lógica para lidar com a seleção do item do dropdown
   };
+
+
+
 
   const menuOptions = [
     { key: 'monofasico', value: 'monofasico', text: 'Monofásico' },
@@ -29,29 +38,49 @@ function ProductCategory() {
     { key: 'aneis', value: 'aneis', text: 'Anéis' },
   ];
 
+
+
+
+
+
+
+
   const teste = () => {
     console.log('aaaaa')
   };
 
+
+
+
   const numero = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-  const verify = () => {
-    const Registered = localStorage.getItem('verifyLogin');
-    if (Registered === "yes") {
-      return true
-    } else {
-      return false
-    }
-  }
 
+
+
+  // useEffect(() => {
+  //   verify();
+  // }, []);
+
+
+
+
+  // const verify = () => {
+  //   console.log(localStorage.getItem("verificar"));
+  //   if (
+  //     localStorage.getItem("verificar") === "sim" ||
+  //     localStorage.getItem("verificar2") === "sim"
+  //   ) {
+  //     setVerificacao(true);
+  //   }
+  // };
   return (
     <>
-      {!verify() ? <Header /> : <HeaderLogin />}<WeggnerModal />
-      <div class="ui gridProduct">
-        <div class="two column row page">
-          <div class="row filter">
+      <Header />
+      <div class="ui grid category">
+        <div class="two column row category">
+          <div class="ui row category">
             <div class="column filter">
-              <div className='second-filter '>
+              <div className="second-filter">
                 <img className="iconMotor" src={iconMotor} />
                 <h1 className="ui header motor">MOTORES</h1>
                 <br />
@@ -85,46 +114,61 @@ function ProductCategory() {
                 </div>
                 <br />
                 <br />
-
-              </div>
-            </div>
-          </div>
-
-          <div class="row itens">
-
-            <div class="column itens">
-              <Link to="/product"> </Link>
-              <div id="produtos">
-
-                {numero.map((i) => (
-
-                  <div class="ui segment itens" id="item" key={i}>
-                    <Link to="/product">
-                      <div class="itemContent" style={{ color: 'black' }}>
-                        <div id="imgItens">
-                          <img src={motors} width="125" height="" />
-                        </div>
-                        <div class="itemInfo">
-                          <h2  id="itemNome">W12 Monofásico</h2>
-                          <h4 id="descricao">Desenvolvido para oferecer versatilidade e eficiência.</h4>
-                          <h2 id="itemPreco">R$ 495<sup> 99</sup></h2>
-                        </div>
-                      </div>
-                    </Link>
-                    <button className="save" href="">
-                      <img onClick={teste} src={save} className="salvos" width="40" height="40" id="salvos" />
-                    </button>
-                  </div>
-                ))}
-              </div>
+             
             </div>
           </div>
         </div>
-      </div >
-      <Footer />
+
+
+
+
+        <div class="ui row category">
+
+
+
+
+          <div class="column itens">
+ 
+            <div id="produtos">
+
+
+
+
+              {numero.map((i) => (
+               
+                <div class="ui segment itens categoryItens" id="itemCategory" key={i}>
+                  <Link to="/product">
+                  <div class="itemContentCategory" style={{color:'black'}}>
+                    <div id="imgItensCategory">
+                      <img src={motors} width="125" height="" />
+                    </div>
+                    <div class="itemInfoCategory">
+                    <h2 id="itemNameCategory">W12 Monofásico</h2>
+                      <h4 id="descricaoCategory">Desenvolvido para oferecer versatilidade e eficiência.</h4>
+                      <h2 id="itemPrecoCategory">R$ 495<sup> 99</sup></h2>
+                    </div>
+                  </div>
+                  </Link>
+                  <button className="saveCategory" href="">
+                        <img onClick={teste} src={save} className="salvosCategory" width="40" height="40" id="salvos" />
+                  </button>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div >
+    <Footer />
     </>
   );
 };
+
+
+
+
+
+
 
 
 export default ProductCategory
