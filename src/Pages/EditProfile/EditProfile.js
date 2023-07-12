6d2bceab7ac3a4e48caf8f04c072dbd8873228ff
mary <105462946+mariazinhagameplys@users.noreply.github.com>
2023-07-12 16:55:56 -0300

    att

---
+++ src/Pages/EditProfile/EditProfile.js
@@ -0,0 +1,79 @@
+import React, { useEffect, useState } from 'react'
+import './EditProfile.css'
+import motor from "../../assets/img/motor.png"
+import { CgProfile } from 'react-icons/cg'
+import { FaRegEdit } from 'react-icons/'
+import { IoMdAddCircle } from 'react-icons/io'
+import Header from '../../Components/Header/Header'
+import Footer from '../../Components/Footer/Footer'
+import HeaderLogin from '../../Components/HeaderLogin/HeaderLogin'
+import WeggnerModal from '../../Components/WeggnerModal/WeggnerModal'
+
+
+function EditProfile() {
+    const [productsCompared, setItems] = useState([]);
+
+    useEffect(() => {
+        const productComparedList = JSON.parse(localStorage.getItem('productsCompared'));
+        if (productComparedList) {
+            setItems(productComparedList);
+        }
+    }, []);
+
+
+    const verifyHeader = () => {
+        const Registered = localStorage.getItem('verifyLogin');
+        if (Registered === "yes") {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    return (
+        <>{!verifyHeader() ? <Header /> : <HeaderLogin />}<WeggnerModal />
+            <div className='containerEdit'>
+                <div class="ui grid edit">
+                    <div class="two column row edit">
+                        <div class="ui row edit">
+                            <div class="column edit">
+                                <div className="settingsBarEdit">
+                                    <div className='mainInfoEdit'>
+                                        <CgProfile className='profileEdit'></CgProfile>
+                                        {/* <CgEdit className='editIcon'></CgEdit> */}
+                                        <h4>Nome Sobrenome</h4>
+                                        <p>000.000.000-00</p>
+                                    </div>
+                                    <div class="ui vertical menu edit">
+                                        <a class="item">
+                                            <p className='titleSectionEdit'>Perfil</p>
+                                        </a>
+                                        <a class="item">
+                                            <p className='titleSectionEdit'>Mensagens</p>
+                                        </a>
+                                        <a class="item">
+                                            <p className='titleSectionEdit'>Endereço</p>
+                                        </a>
+                                        <a class="item">
+                                            <p className='titleSectionEdit'>Avaliações</p>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div >
+                </div >
+                <div class="ui row editSection">
+                    <div class="column sections">
+                        <div className='edit-section'>
+                            <h2 className='edit-section-title'>Informações Pessoais</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Footer />
+        </>
+    )
+}
+
+export default EditProfile
--- src/Service/user-service.js
+++ src/Service/user-service.js
@@ -4,29 +4,39 @@ const url = 'http://localhost:8081/user'
 
 export const UserService = {
 
-    create: function(user) {
-        console.log(user)
-        axios.post(url + '/create', user).then((response) => {
-            return response.data;
-          });
-    },
+  create: function (user) {
+    console.log(user)
+    axios.post(url + '/create', user).then((response) => {
+      return response.data;
+    });
+  },
 
-    getAll: function() {
-        return new Promise((resolve, reject) => {
-          axios.get(url + '/getAll')
-            .then((response) => {
-              resolve(response.data);
-            })
-            .catch((error) => {
-              reject(error);
-            });
+  getAll: function () {
+    return new Promise((resolve, reject) => {
+      axios.get(url + '/getAll')
+        .then((response) => {
+          resolve(response.data);
+        })
+        .catch((error) => {
+          reject(error);
         });
-      },
+    });
+  },
 
-    getOne: function(id){
-        axios.post(url + '/getOne/', id).then((response) => {
-            return response.data;
-          });
-    }
+  getOne: function (id) {
+    axios.post(url + '/getOne/', id).then((response) => {
+      return response.data;
+    });
+  },
+  edit: function (id, user) {
+    axios.put(url + '/edit/' + id, user).then((response) => {
+      return response.data
+    })
+  },
+  delete: function(id){
+    axios.post(url + '/delete/'+ id).then((response) => {
+        return response.data;
+      });
+}
 
 }
