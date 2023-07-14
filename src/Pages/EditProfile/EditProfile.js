2bec6116043e2dc56530535799df40075e7a8057
Gustavo Guilherme de Abreu <110638920+Fennek01@users.noreply.github.com>
2023-07-14 17:56:25 -0300

    ATT

--- src/Pages/EditProfile/EditProfile.js
+++ src/Pages/EditProfile/EditProfile.js
@@ -10,6 +10,7 @@ import RatedProducts from '../../Components/RatedProducts/RatedProducts';
 import HeaderLogin from '../../Components/HeaderLogin/HeaderLogin';
 import WeggnerModal from '../../Components/WeggnerModal/WeggnerModal';
 import { FaStar } from 'react-icons/fa';
+import { CiSquareAlert } from 'react-icons/ci';
 import ProductCarouselSmallSimilar from '../../Components/ProductCarouselSmallSimilar/ProductCarouselSmallSimilar';
 
 
@@ -79,56 +80,221 @@ function EditProfile() {
                 <form class="ui form edit">
                   <div class="field">
                     <div class="two fields">
-                      <div class="field disabled">
+                      <div class={editMode ? "field" : "field disabled"}>
                         <label>Nome</label>
-                        <input style={{ border: editMode ? '1px solid #000' : 'none' }} type="text" name="shipping[first-name]" placeholder="Primiero Nome" />
+                        <input style={{ border: editMode ? '1px solid #000' : 'none' }} type="text" name="shipping[first-name]" placeholder="Gustavo" />
                       </div>
-                      <div class="field disabled">
+                      <div class={editMode ? "field" : "field disabled"}>
                         <label>Sobrenome</label>
-                        <input style={{ border: editMode ? '1px solid #000' : 'none' }} type="text" name="shipping[last-name]" placeholder="Rua, Bairro, Número" />
+                        <input style={{ border: editMode ? '1px solid #000' : 'none' }} type="text" name="shipping[last-name]" placeholder="Abreu" />
                       </div>
                     </div>
                   </div>
                   <div class="field">
                     <div class="fields">
-                      <div class="twelve wide field disabled">
+                      <div class={editMode ? "twelve wide field" : "twelve wide field disabled"}>
                         <label>Email Profissional</label>
-                        <input style={{ border: editMode ? '1px solid #000' : 'none' }} type="text" name="shipping[address]" placeholder="Complemento" />
+                        <input style={{ border: editMode ? '1px solid #000' : 'none' }} type="text" name="shipping[address]" placeholder="carlosabreu@gmail.com" />
                       </div>
-                      <div class="four wide field disabled">
+                      <div class={editMode ? "four wide field" : "four wide field disabled"}>
                         <label>CPF/CNPJ</label>
-                        <input style={{ border: editMode ? '1px solid #000' : 'none' }} type="text" name="shipping[address-2]" placeholder="CEP " />
+                        <input style={{ border: editMode ? '1px solid #000' : 'none' }} type="text" name="shipping[address-2]" placeholder="076.137.949-54" />
                       </div>
                     </div>
                   </div>
+                  <div className="buttonsProfileEdit">
+                    {!editMode ? (
+                      <div className='editButton'>
+                        <button onClick={changeInfo} className="fluid ui button edit">Editar</button>
+                      </div>
+                    ) : (
+                      <div className='cancelButton'>
+                        <button onClick={changeInfo} className="fluid ui button cancel">Cancelar</button>
+                      </div>
+                    )
+                    }
+                    {!editMode ? (
+                      <div>
+                      </div>
+                    ) : (
+                      <div className='conclusionButton'>
+                        <button onClick={changeInfo} className="fluid ui button conclusion">Concluir</button>
+                      </div>
+                    )}
+                  </div>
                 </form>
-                <button onClick={changeInfo} className="ui button edit">Editar</button>
+                <div className='containerInformationProfile'>
+                  {editMode ? (
+                    <CiSquareAlert className='iconInformationProfileAlert' />
+                  ) : (
+                    <CiSquareAlert className='iconInformationProfile' />
+                  )
+                  }
+                  <p>
+                    Para garantir a precisão e integridade das informações,
+                    pedimos que verifique cuidadosamente todos os dados antes de
+                    concluir qualquer edição em sua conta de e-commerce. Lembre-se de que alterações
+                    incorretas ou incompletas podem afetar negativamente sua experiência de compra e o
+                    processamento adequado dos pedidos.
+                  </p>
+                </div>
               </div>
             </div>
           ) : activeSection === 2 ? (
             <div className="column sections">
               <div className="edit-section-profile">
                 <h2 className="edit-section-title-profile">Mensagens</h2>
-                <Messages/>
+                <div className='messagesContainer'>
+                  <Messages />
+                  <Messages />
+                  <Messages />
+                  <Messages />
+                  <Messages />
+                  <Messages />
+                </div>
               </div>
 
             </div>
           ) : activeSection === 3 ? (
             <div className="column sections">
               <div className="edit-section-profile">
-                <h2 className="edit-section-title-profile">Endereço</h2>
+                <h2 className="edit-section-title-profile">Endereço Registrado</h2>
+                <form class="ui form edit">
+                  <div class="field">
+                    <div class="two fields">
+                      <div class={editMode ? "twelve wide field" : "twelve wide field disabled"}>
+                        <label>Complemento</label>
+                        <input style={{ border: editMode ? '1px solid #000' : 'none' }} type="text" name="shipping[first-name]" placeholder="Complemento" />
+                      </div>
+                      <div class={editMode ? "four wide field" : "four wide field disabled"}>
+                        <label>CEP</label>
+                        <input style={{ border: editMode ? '1px solid #000' : 'none' }} type="text" name="shipping[last-name]" placeholder="89256-890" />
+                      </div>
+                    </div>
+                  </div>
+                  <div class="field">
+                    <div class="fields">
+                      <div class={editMode ? "twelve wide field" : "twelve wide field disabled"}>
+                        <label>Endereço</label>
+                        <input style={{ border: editMode ? '1px solid #000' : 'none' }} type="text" name="shipping[address]" placeholder="Rua, Bairro, Número" />
+                      </div>
+                      <div class={editMode ? "four wide field" : "four wide field disabled"}>
+                        <label>Estado</label>
+                        <select class="ui fluid dropdown">
+                          <option value="">Estado</option>
+                          <option value="AC">Acre</option>
+                          <option value="AL">Alagoas</option>
+                          <option value="AP">Amapá</option>
+                          <option value="AM">Amazonas</option>
+                          <option value="BA">Bahia</option>
+                          <option value="CE">Ceará</option>
+                          <option value="DF">Distrito Federal</option>
+                          <option value="ES">Espírito Santo</option>
+                          <option value="GO">Goiás</option>
+                          <option value="MA">Maranhão</option>
+                          <option value="MT">Mato Grosso</option>
+                          <option value="MS">Mato Grosso do Sul</option>
+                          <option value="MG">Minas Gerais</option>
+                          <option value="PA">Pará</option>
+                          <option value="PB">Paraíba</option>
+                          <option value="PR">Paraná</option>
+                          <option value="PE">Pernambuco</option>
+                          <option value="PI">Piauí</option>
+                          <option value="RJ">Rio de Janeiro</option>
+                          <option value="RN">Rio Grande do Norte</option>
+                          <option value="RS">Rio Grande do Sul</option>
+                          <option value="RO">Rondônia</option>
+                          <option value="RR">Roraima</option>
+                          <option value="SC">Santa Catarina</option>
+                          <option value="SP">São Paulo</option>
+                          <option value="SE">Sergipe</option>
+                          <option value="TO">Tocantins</option>
+                        </select>
+                      </div>
+                      <div class={editMode ? "four wide field" : "four wide field disabled"}>
+                        <label>País</label>
+                        <select class="ui fluid dropdown">
+                          <option value="">País</option>
+                          <option value="ZA">África do Sul</option>
+                          <option value="DE">Alemanha</option>
+                          <option value="AE">Emirados Árabes Unidos</option>
+                          <option value="AR">Argentina</option>
+                          <option value="DZ">Argélia</option>
+                          <option value="AU">Austrália</option>
+                          <option value="BR">Brasil</option>
+                          <option value="BE">Bélgica</option>
+                          <option value="CA">Canadá</option>
+                          <option value="KZ">Cazaquistão</option>
+                          <option value="CL">Chile</option>
+                          <option value="CN">China</option>
+                          <option value="CO">Colômbia</option>
+                          <option value="KR">Coreia do Sul</option>
+                          <option value="AE">Emirados Árabes Unidos</option>
+                          <option value="ES">Espanha</option>
+                          <option value="US">Estados Unidos</option>
+                          <option value="FR">França</option>
+                          <option value="IN">Índia</option>
+                          <option value="IT">Itália</option>
+                          <option value="MX">México</option>
+                          <option value="PT">Portugal</option>
+                          <option value="GB">Reino Unido</option>
+                          <option value="TR">Turquia</option>
+
+                          , Equador, Espanha, Estados Unidos, França, Gana, Itália, Japão, Malásia, Países Baixos, Peru, Polônia, Reino Unido, Rússia,[a] Singapura, Suécia, Tailândia, Turquia e Índia, além de 57 filiais[24] e distribuição em mais de 135 países.
+                        </select>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="buttonsProfileEdit">
+                    {!editMode ? (
+                      <div className='editButton'>
+                        <button onClick={changeInfo} className="fluid ui button edit">Editar</button>
+                      </div>
+                    ) : (
+                      <div className='cancelButton'>
+                        <button onClick={changeInfo} className="fluid ui button cancel">Cancelar</button>
+                      </div>
+                    )
+                    }
+                    {!editMode ? (
+                      <div>
+                      </div>
+                    ) : (
+                      <div className='conclusionButton'>
+                        <button onClick={changeInfo} className="fluid ui button conclusion">Concluir</button>
+                      </div>
+                    )}
+                  </div>
+                </form>
+                <div className='containerInformationProfile'>
+                  {editMode ? (
+                    <CiSquareAlert className='iconInformationProfileAlert' />
+                  ) : (
+                    <CiSquareAlert className='iconInformationProfile' />
+                  )
+                  }
+                  <p>
+                    Para garantir a precisão e integridade das informações,
+                    pedimos que verifique cuidadosamente todos os dados antes de
+                    concluir qualquer edição em sua conta de e-commerce. Lembre-se de que alterações
+                    incorretas ou incompletas podem afetar negativamente sua experiência de compra e o
+                    processamento adequado dos pedidos.
+                  </p>
+                </div>
               </div>
             </div>
           ) : activeSection === 4 ? (
             <div className="column sections">
-              <div className="edit-section-ratings">
+              <div className="edit-section-profile">
                 <h2 className="edit-section-title-profile">Avaliações</h2>
-                <RatedProducts />
-                <RatedProducts />
+                <div className='ratingContainer'>
+                  <RatedProducts />
+                  <RatedProducts />
+                </div>
               </div>
             </div>
           ) : null}
-        </div>
+        </div >
       </div >
       <div className='boxTitleSimilarCompare'>
         <FaStar color='var(--white)' size={40} />
