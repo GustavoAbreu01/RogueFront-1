a28f2432fc7a4d4ed9f92df829636142d72b97ce
Gustavo Guilherme de Abreu <110638920+Fennek01@users.noreply.github.com>
2023-07-17 16:51:58 -0300

    ATT

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,22 +1,25 @@
 import React from 'react'
 import './NotFound.css'
+// importando imagens
 import weggner from "../../assets/img/NotFoundWeggner.gif"
-import Header from '../../Components/Header/Header'
-import Footer from '../../Components/Footer/Footer'
+
+// importando icones
 import { BsArrowLeftShort } from 'react-icons/bs'
+
+// importando framework
 import { Link } from 'react-router-dom'
 
 
 function NotFound() {
   return (
     <>
-      <div className='ContainerNotFound'>
-        <img src={weggner} alt="weggner" className="imgNotFound" />
-        <h2 className="textTitleNotFound">Página não encontrada</h2>
-        <p className="textNotFound">A página que você está procurando não existe ou foi removida.</p>
-        <div className='boxCartProductSubTextFinal'>
+      <div className='container_not_found'>
+        <img src={weggner} alt="weggner" className="not_found_img" />
+        <h2 className="not_found_title">Página não encontrada</h2>
+        <p>A página que você está procurando não existe ou foi removida.</p>
+        <div className='not_found_text'>
           <BsArrowLeftShort size={15} />
-          <Link to='/'> <p className='SubTextFinal'>Voltar para a Home</p> </Link>
+          <Link to='/'> <p>Voltar para a Home</p> </Link>
         </div>
       </div>
     </>
