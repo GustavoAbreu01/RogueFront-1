5db33f6bdd4e4dc664ddf13b2e0b7e477b97e1ed
Gustavo Guilherme de Abreu <110638920+Fennek01@users.noreply.github.com>
2023-07-17 16:39:50 -0300

    ATT

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,13 +1,7 @@
 import React from 'react'
-import WeggnerModal from '../../Components/WeggnerModal/WeggnerModal'
-import questIcon from "../../assets/img/questionario.png"
-import compare from '../../assets/img/compare.png'
-import weggner from '../../assets/img/WagnerDormindo.png'
-import Footer from '../../Components/Footer/Footer'
-import Header from '../../Components/Header/Header'
-import HeaderLogin from '../../Components/HeaderLogin/HeaderLogin'
-import Carousell from '../../Components/carousel/carousel'
-import logo from "../../assets/img/logo-weg.png"
+import './Home.css'
+
+//Importando as imagens
 import iconMotor from "../../assets/img/iconeMotor.png"
 import Qualidade from "../../assets/img/MOTORES-03.png"
 import Compatibilidade from "../../assets/img/COMPATIBILIDADE-03.png"
@@ -16,9 +10,15 @@ import Inovação from "../../assets/img/ESTACOES_DE_RECARGA-03.png"
 import Crescimento from "../../assets/img/GRAFICO_PRINCIPAL-03.png"
 import Assistência from "../../assets/img/CALL_CENTER-03.png"
 import IoT from "../../assets/img/PLATAFORMA_IOT_WEGNOLOGY-03.png"
-import salvo from "../../assets/img/salvo.png"
 import BannerAnnouncementOne from '../../assets/img/BannerAnnun1.jpg'
 import BannerAnnouncementTwo from '../../assets/img/BannerAnnun2.jpg'
+import motor from "../../assets/img/motor.png"
+
+//importando as frameworks
+import Aos from 'aos'
+import { Link } from 'react-router-dom'
+
+//Importando os icones
 import { BiLineChart } from 'react-icons/bi'
 import { MdOutlineSecurity } from 'react-icons/md'
 import { BsPaintBucket } from 'react-icons/bs'
@@ -28,14 +28,20 @@ import { LiaCertificateSolid } from 'react-icons/lia'
 import { MdOutlineNewReleases } from 'react-icons/md'
 import { TfiPanel } from 'react-icons/tfi'
 import { FaStar } from 'react-icons/fa'
-import './Home.css'
-import motor from "../../assets/img/motor.png"
-import { Link } from 'react-router-dom'
+
+//Importando os componentes
 import ProductCarousel from '../../Components/ProductCarousel/ProductCarousel'
 import ProductHighlightCarousel from '../../Components/ProductHighlightCarousel/ProductHighlightCarousel'
-import Aos from 'aos'
 import ProductSearchedCarousel from '../../Components/ProductSearchedCarousel/ProductSearchedCarousel'
 import ProductNewCarousel from '../../Components/ProductNewCarousel/ProductNewCarousel'
+import WeggnerModal from '../../Components/WeggnerModal/WeggnerModal'
+import questIcon from "../../assets/img/questionario.png"
+import compare from '../../assets/img/compare.png'
+import weggner from '../../assets/img/WagnerDormindo.png'
+import Footer from '../../Components/Footer/Footer'
+import Header from '../../Components/Header/Header'
+import HeaderLogin from '../../Components/HeaderLogin/HeaderLogin'
+import Carousell from '../../Components/carousel/carousel'
 
 
 function Home() {
@@ -143,173 +149,174 @@ function Home() {
     <div>
       <>{!verify() ? <Header /> : <HeaderLogin />}
         <WeggnerModal />
-        <div className='boxCarrossel'>
+        <div className='home_carousel_banner'>
           <Carousell></Carousell>
         </div>
-        <div className="dividerCarrossel"></div>
-        <div className="optionPages">
+        <div className="home_divider_carousel"></div>
+        <div className="box_home_option_pages">
           <Link to='/quiz' style={{ textDecoration: 'none' }}>
-            <div className="optionQuest">
-              <div className="questIcon">
+            <div>
+              <div className="home_optional_icons">
                 <img src={questIcon} alt="" width={75} />
                 <div>
-                  <h4 className='titleOpc'>Descubra qual é<br />
+                  <h4 className='home_optional_title'>Descubra qual é<br />
                     o produto para você!</h4>
-                  <p className='descOpc'>Faça o nosso questionário</p>
+                  <p className='home_optional_description'>Faça o nosso questionário</p>
                 </div>
               </div>
             </div>
           </Link>
-          <div className='dividerIcons'></div>
+          <div className='home_divider_icons'></div>
           <Link to='/compare' style={{ textDecoration: 'none' }}>
-            <div className="optionQuest">
-              <div className="questIcon">
+            <div>
+              <div className="home_optional_icons">
                 <img src={compare} alt="" width={150} />
                 <div>
-                  <h4 className='titleOpc'>Compare produtos</h4>
-                  <p className='descOpc'>Veja qual se encaixa melhor<br />
+                  <h4 className='home_optional_title'>Compare produtos</h4>
+                  <p className='home_optional_description'>Veja qual se encaixa melhor<br />
                     com a sua necessidade</p>
                 </div>
               </div>
             </div>
           </Link>
-          <div className='dividerIcons'></div>
-          <div className="optionWeggner">
-            <div className="questIcon">
+          <div className='home_divider_icons'></div>
+          <div>
+            <div className="home_optional_icons">
               <img src={weggner} alt="" width={125} />
               <div>
-                <h4 className='titleOpc'>Conheça o nosso<br />
+                <h4 className='home_optional_title'>Conheça o nosso<br />
                   Assistente Virtual!</h4>
-                <p className='descOpc'>Navegar ficou ainda mais fácil</p>
+                <p className='home_optional_description'>Navegar ficou ainda mais fácil</p>
               </div>
             </div>
           </div>
         </div>
-        <div className='boxAccessed'>
-          <div className='boxTitleAccessed'>
+        <div>
+          <div className='home_accessed'>
             <BiLineChart color='var(--blue-primary)' size={50} />
-            <h1 className='titleAccessed' >Produtos mais acessados</h1>
+            <h1 className='home_accessed_title' >Produtos mais acessados</h1>
           </div>
-          <div className='sliderProductAccessed'>
+          <div className='home_slider_product_accessed'> 
             <ProductCarousel />
           </div>
         </div>
 
-        <div className='boxHighlights' >
-          <div className='boxTitleHighlights'>
+        <div className='home_highlights' >
+          <div className='home_highlights_title'>
             <FaStar color='var(--white)' size={40} />
-            <h1 className='titleHighlights' >Destaques da semana</h1>
+            <h1 className='home_highlights_title_text' >Destaques da semana</h1>
           </div>
-          <div className='boxHighlightsProducts'>
+          <div className='home_slider_product_highlights'>
             <ProductHighlightCarousel />
           </div>
         </div>
 
-        <div className='boxCategoryButtons' >
-          <h1 className='categoryTitleBox'>Departamentos</h1>
-          <div className='categoryButtonsOpt'>
+        <div className='container_home_category_buttons' >
+          <h1 className='home_category_title'>Departamentos</h1>
+          <div className='box_home_category_buttons'> 
             <Link to='/category'>
-              <div className='categoryTitle'>
-                <button className='categoryButton'>
-                  <MdOutlineSecurity color='var(--blue-primary)' className='categoryButtonImg' ></MdOutlineSecurity>
+              <div className='home_optional_category'>
+                <button className='home_optional_category_button'>
+                  <MdOutlineSecurity color='var(--blue-primary)' className='home_optional_category_button_image' ></MdOutlineSecurity>
                 </button>
-                <h2>Segurança</h2>
+                <h2 className='home_optional_category_title'>Segurança</h2>
               </div>
             </Link>
             <Link to='/category'>
-              <div className='categoryTitle'>
-                <button className='categoryButton'>
-                  <BsPaintBucket color='var(--blue-primary)' className='categoryButtonImg' ></BsPaintBucket>
+              <div className='home_optional_category'>
+                <button className='home_optional_category_button'>
+                  <BsPaintBucket color='var(--blue-primary)' className='home_optional_category_button_image' ></BsPaintBucket>
                 </button>
-                <h2>Tintas</h2>
+                <h2 className='home_optional_category_title'>Tintas</h2>
               </div>
             </Link>
             <Link to='/category'>
-              <div className='categoryTitle'>
-                <button className='categoryButton'>
-                  <CgSmartphoneChip color='var(--blue-primary)' className='categoryButtonImg' ></CgSmartphoneChip>
+              <div className='home_optional_category'>
+                <button className='home_optional_category_button'>
+                  <CgSmartphoneChip color='var(--blue-primary)' className='home_optional_category_button_image' ></CgSmartphoneChip>
                 </button>
-                <h2>Automação</h2>
+                <h2 className='home_optional_category_title'>Automação</h2>
               </div>
             </Link>
             <Link to='/category'>
-              <div className='categoryTitle'>
-                <button className='categoryButton'>
-                  <TfiPanel color='var(--blue-primary)' className='categoryButtonImg'></TfiPanel>
+              <div className='home_optional_category'>
+                <button className='home_optional_category_button'>
+                  <TfiPanel color='var(--blue-primary)' className='home_optional_category_button_image'></TfiPanel>
                 </button>
-                <h2>Painéis</h2>
+                <h2 className='home_optional_category_title'>Painéis</h2>
               </div>
             </Link>
             <Link to='/category'>
-              <div className='categoryTitle'>
-                <button className='categoryButton'>
+              <div className='home_optional_category'>
+                <button className='home_optional_category_button'>
                   <img src={iconMotor} width={75} height={50} alt=''></img>
                 </button>
-                <h2>Motores</h2>
+                <h2 className='home_optional_category_title'>Motores</h2>
               </div>
             </Link>
           </div>
         </div>
 
-        <div className='boxSearched' >
-          <div className='containerTitleSearched'>
-            <div className='boxTitleSearched'>
-              <h1 className='titleSearched' >Mais procurados</h1>
+        <div className='home_searched' >
+          <div className='home_searched_title'>
+            <div className='home_searched_aling'>
+              <h1 className='home_searched_title_text' >Mais procurados</h1>
               <CgSearchFound color='var(--white)' size={50} />
             </div>
           </div>
-          <div className='boxSearchedProducts'>
+          <div className='home_slider_product_searched'> 
             <ProductSearchedCarousel />
           </div>
         </div>
 
-        <div className='boxBenefits'>
-          <div className='boxBenefiitsTitle'>
+        <div> 
+          <div className='home_benefiits'>
             <LiaCertificateSolid color='var(--blue-primary)' size={50} />
-            <h1 className='benefiitsTitle'>Benefícios de compra</h1>
-          </div>
-          <div className="containerBenefits">
+            <h1 className='home_benefiits_title'>Benefícios de compra</h1>
+          </div> 
+
+          <div className="container_home_benefits">
             {iconsInfoOne.map((icon) => (
-              <div className="infoBox" data-aos="fade-up" key={icon.id}>
-                <div className="iconInfoBox">
-                  <img src={icon.icon} className="iconInfo" alt='' />
+              <div className="home_benefits_info" data-aos="fade-up" key={icon.id}>
+                <div className="home_benefits_info_icon">
+                  <img src={icon.icon} className="home_benefits_icon" alt='' />
                 </div>
-                <div className='infoDescriptionBox'>
-                  <h1 className="titleInfo" >{icon.name}</h1>
-                  <p className="descriptionInfo">{icon.description}</p>
-                </div>
+                <div className='home_benefits_description_info'>
+                  <h1 className="home_benefits_description_title" >{icon.name}</h1>
+                  <p className="home_benefits_description">{icon.description}</p>
+                </div> 
               </div>
             ))}
           </div>
 
-          <div className="containerBenefits">
+          <div className="container_home_benefits">
             {iconsInfoTwo.map((icon) => (
-              <div className="infoBox" data-aos="fade-up" key={icon.id}>
-                <div className="iconInfoBox">
-                  <img src={icon.icon} className="iconInfo" alt='' />
+              <div className="home_benefits_info" data-aos="fade-up" key={icon.id}>
+                <div className="home_benefits_info_icon">
+                  <img src={icon.icon} className="home_benefits_icon" alt='' />
                 </div>
-                <div className='infoDescriptionBox'>
-                  <h1 className="titleInfo" >{icon.name}</h1>
-                  <p className="descriptionInfo" >{icon.description}</p>
+                <div className='home_benefits_description_info'>
+                  <h1 className="home_benefits_description_title" >{icon.name}</h1>
+                  <p className="home_benefits_description" >{icon.description}</p>
                 </div>
               </div>
             ))}
           </div>
         </div>
 
-        <div className='boxHighlights' >
-          <div className='boxTitleHighlights'>
+        <div className='home_highlights' >
+          <div className='home_highlights_title'>
             <MdOutlineNewReleases color='var(--white)' size={40} />
-            <h1 className='titleHighlights' >Novidades</h1>
+            <h1 className='home_highlights_title_text' >Novidades</h1>
           </div>
-          <div className='boxHighlightsProducts'>
+          <div className='home_slider_product_highlights'>
             <ProductNewCarousel />
           </div>
         </div>
 
-        <div className='boxAnnouncements' data-aos="fade-up" >
-          <img className='announBanner' src={BannerAnnouncementOne} />
-          <img className='announBanner' src={BannerAnnouncementTwo} />
+        <div className='home_announcements' data-aos="fade-up" >
+          <img className='home_announcements_banner' src={BannerAnnouncementOne} />
+          <img className='home_announcements_banner' src={BannerAnnouncementTwo} />
         </div>
 
         <Footer /></>
