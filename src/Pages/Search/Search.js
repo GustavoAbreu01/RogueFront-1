e0ca19e6de22926207b5e7f8c530849b05693b0a
Inácio Z. Vitkoski <65171054+inaciozacko@users.noreply.github.com>
2023-07-19 14:38:44 -0300

    1º Commit

--- src/Pages/Search/Search.js
+++ src/Pages/Search/Search.js
@@ -25,38 +25,38 @@ function Search() {
   return (
     <div>
       {!verify() ? <Header /> : <HeaderLogin />}
-      <div className="ui grid search">
-        <div className="two column row search">
+      <div className="ui grid search container_searchBar">
+        <div className="two column row search box_searchBar">
           <div className="ui row search">
-            <div className="column filter">
-              <div className="search-filter">
-                <h1 className="ui header filter">FILTROS</h1>
+            <div className="column filter searchBar_filter">
+              <div className="search-filter searchBar_filter">
+                <h1 className="ui header filter searchBar_filter">FILTROS</h1>
                 <br />
-                <div class="ui checkbox">
+                <div class="ui checkbox searchBar_checkbox">
                   <input className="check" type="checkbox" id="monofasico" name="monofasico" value="Monofasico" />
                   <label className="desc" htmlFor="Monofasico"> Monofásico </label>
                 </div>
                 <br />
                 <br />
-                <div class="ui checkbox">
+                <div class="ui checkbox searchBar_checkbox">
                   <input className="check" type="checkbox" id="trifasicoBT" name="trifasicoBT" value="trifasicoBT" />
                   <label className="desc" htmlFor="TrifasicoBT">Trifásico - Baixa Tensão</label>
                 </div>
                 <br />
                 <br />
-                <div class="ui checkbox">
+                <div class="ui checkbox searchBar_checkbox">
                   <input className="check" type="checkbox" id="trifasicoAT" name="trifasicoAT" value="trifasicoAT" />
                   <label className="desc" htmlFor="TrifasicoAT">Trifásico - Alta Tensão</label>
                 </div>
                 <br />
                 <br />
-                <div class="ui checkbox">
+                <div class="ui checkbox searchBar_checkbox">
                   <input className="check" type="checkbox" id="sincronos" name="sincronos" value="sincronos" />
                   <label className="desc" htmlFor="Sincronos">Síncronos</label>
                 </div>
                 <br />
                 <br />
-                <div class="ui checkbox">
+                <div class="ui checkbox searchBar_checkbox" >
                   <input className="check" type="checkbox" id="aneis" name="aneis" value="aneis" />
                   <label className="desc" htmlFor="Aneis">Anéis</label>
                 </div>
@@ -64,7 +64,7 @@ function Search() {
               </div>
             </div>
           </div>
-          <div class="ui row searchItens">
+          <div class="ui row searchItens searchBar_checkbox ">
             <div class="column itens">
               <div id="produtos" className="ui grid searchItens">
                 {numero.map((i) => (
