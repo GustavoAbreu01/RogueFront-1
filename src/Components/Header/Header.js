71cd857146a9ba82c7b974cce64d6751bc465062
Gustavo Guilherme de Abreu <110638920+Fennek01@users.noreply.github.com>
2023-07-26 14:21:11 -0300

    ATT

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,16 +1,35 @@
 import React, { useEffect, useState } from 'react'
 import './Header.css'
 import { FaShoppingCart } from 'react-icons/fa'
+import { BsFillBookmarkFill } from 'react-icons/bs'
 import logo from "../../assets/img/logo-weg.png"
 import { Link } from 'react-router-dom'
 import SearchBar from '../SearchModal/SearchModal'
 import MenuModal from '../MenuModal/MenuModal'
+import PerfilModal from '../ProfileModal/ProfileModal'
+import { UserService } from '../../Service'
 
+function HeaderLogin() {
 
-
-function Header() { 
-
+  const [user, setUser] = useState({});
   const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        let users = await UserService.getAll();
+        users.forEach((user1) => {
+          if (user1.register === JSON.parse(localStorage.getItem('userCpf'))) {
+            setUser(user1)
+          }
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    fetchData();
+  }, []);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -27,35 +46,37 @@ function Header() {
 
   return (
     <><header className={scrolled ? 'scrolled' : ''}>
-      <div className="header_img">
-        <Link to={"/"}><img className='header_imgHeader' src={logo} alt="" /></Link>
+      <div className="img">
+        <Link to="/"><img src={logo} alt="" /></Link>
       </div>
       <div className="searchInput">
         <SearchBar />
       </div>
-      <div className="box_header_opcHeader">
-        <div className="header_opcDeEntradaHeader">
-          <Link className="header_accountHeader" to="/register">Crie sua conta</Link>/<Link className="loginHeader" to="/register">Login</Link>
+      <div className="opc">
+        <div className='opc_user'>
+          <Link to="/register "><p className='opc_login'>Login</p></Link><Link to="/register "><p className='opc_register'>Realizar Cadastro</p></Link>
         </div>
-        <Link to="/cart"><FaShoppingCart className="header_carrinhoHeaderDetail"/></Link>
+        <div className='icons'>
+          <Link className="cart" to="/cart"><FaShoppingCart className="carrinho" /></Link>
+        </div>
       </div>
-    </header><div className="box_header_navBarHeader">
-        <div id="header_menu">
+    </header><div className="navBar">
+        <div id="menu">
           <MenuModal />
         </div>
-        <div className="header_divisores"></div>
-        <Link to="/category" >Motores</Link>
-        <div className="header_divisores"></div>
-        <Link to="/category" >Tintas e Vernizes</Link>
-        <div className="header_divisores"></div>
+        <div className="divisores"></div>
+        <Link to="/category">Motores</Link>
+        <div className="divisores"></div>
+        <Link to="/category">Tintas e Vernizes</Link>
+        <div className="divisores"></div>
         <Link to="/category">Automação</Link>
-        <div className="header_divisores"></div>
+        <div className="divisores"></div>
         <Link to="/category">Paineis Eletricos</Link>
-        <div className="header_divisores"></div>
+        <div className="divisores"></div>
         <Link to="/category">Segurança</Link>
-        <div className="header_divisores"></div>
+        <div className="divisores"></div>
       </div></>
   )
 }
 
-export default Header
+export default HeaderLogin
