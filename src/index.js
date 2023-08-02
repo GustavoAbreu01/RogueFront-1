f862707c8708950dddc61622c1afbb8a7fbd4d86
Gustavo Guilherme de Abreu <110638920+Fennek01@users.noreply.github.com>
2023-08-02 13:38:19 -0300

    ATT

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import 'semantic-ui-css/semantic.min.css';
 import 'react-multi-carousel/lib/styles.css';
 import './index.css';
-import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
