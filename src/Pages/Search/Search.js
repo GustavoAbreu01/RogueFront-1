27d0ff021defc6fe5c317fd5c7b25a49d2bc7b85
mary <105462946+mariazinhagameplys@users.noreply.github.com>
2023-08-07 16:34:27 -0300

    att

--- src/Pages/Search/Search.js
+++ src/Pages/Search/Search.js
@@ -22,8 +22,6 @@ function Search() {
   const queryParams = new URLSearchParams(location.search);
   const searchTerm = queryParams.get('query') || '';
 
-
-
   const numero = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
   const [isOpen, setIsOpen] = useState(false);
