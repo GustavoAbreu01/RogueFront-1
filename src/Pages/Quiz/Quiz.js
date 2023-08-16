8cc4a27baa402f5a87eb9793ca1b2b4af4fc4451
Bruneb4s <105462955+Brunebas@users.noreply.github.com>
2023-08-16 16:57:24 -0300

    ATT

--- src/Pages/Quiz/Quiz.js
+++ src/Pages/Quiz/Quiz.js
@@ -30,16 +30,16 @@ function Quiz() {
 
   const [screenSize, setScreenSize] = useState({ width: 0, height: 0 });
 
-    useEffect(() => {
-      function handleResize() {
-        setScreenSize({ width: window.innerWidth, height: window.innerHeight });
-      }
-      window.addEventListener('resize', handleResize);
-      handleResize();
-      return () => {
-        window.removeEventListener('resize', handleResize);
-      };
-    }, []);
+  useEffect(() => {
+    function handleResize() {
+      setScreenSize({ width: window.innerWidth, height: window.innerHeight });
+    }
+    window.addEventListener('resize', handleResize);
+    handleResize();
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   const proximo = () => {
 
@@ -235,19 +235,177 @@ function Quiz() {
     </>
   )
   const renderMobileView = () => (
-    <></>
+    <>
+      {!verifyHeader() ? <Header /> : <HeaderLogin />}<WeggnerModal />
+        <div className="ui mini steps quiz_mobile">
+          <i className="clipboard list icon quiz_mobile"></i>
+          <div className="box step quiz_mobile">
+            <div className="box content quiz_mobile">
+              <div className="quiz_mobile title">I</div>
+            </div>
+          </div>
+          <div className="box step quiz_mobile">
+            <div className="box content quiz_mobile">
+              <div className="quiz_mobile title">II</div>
+            </div>
+          </div>
+          <div className="box step quiz_mobile">
+            <div className="box content quiz_mobile">
+              <div className="quiz_mobile title">III</div>
+            </div>
+          </div>
+          <div className="box step quiz">
+            <div className="box content quiz">
+              <div className="quiz_mobile title">IV</div>
+            </div>
+          </div>
+          <div className="box step quiz">
+            <div className="box content quiz">
+              <div className="quiz_mobile title">V</div>
+            </div>
+          </div>
+        </div>
+      <div className="ui raised very padded text container segment quiz_mobile">
+        <p className="ui blue ribbon label quiz_mobile">Quiz</p>
+        {Question1() && (
+          <div>
+            <div className="ui large header quiz_mobile">Qual é a linha do produto que você deseja?</div>
+            <div className="ui checkbox quiz_mobile">
+              <input className="quiz_check_option_mobile" type="checkbox" id="industrial" name="industrial" value="Industrial" />
+              <label className="quiz_desc_option_mobile" htmlFor="industrial">Industrial</label>
+            </div>
+            <br />
+            <div className="ui checkbox quiz_mobile">
+              <input className="quiz_check_option_mobile" type="checkbox" id="residencial" name="residencial" value="Residencial" />
+              <label className="quiz_desc_option_mobile" htmlFor="residencial">Residencial</label><br />
+            </div>
+          </div>
+        )}
+
+        {Question2() && (
+          <div>
+            <div className="ui large header quiz_mobile">Qual a voltagem necessária para o seu produto?</div>
+
+            <div className="ui checkbox quiz_mobile">
+              <input className="quiz_check_option_mobile" type="checkbox" id="12v" name="12v" value="12v" />
+              <label className="quiz_desc_option_mobile" htmlFor="12v">12V</label>
+            </div>
+            <br />
+            <div className="ui checkbox quiz_mobile">
+              <input className="quiz_check_option_mobile" type="checkbox" id="20V" name="20V" value="20V" />
+              <label className="quiz_desc_option_mobile" htmlFor="20V">20V</label>
+            </div>
+            <br />
+            <div className="ui checkbox quiz_mobile">
+              <input className="quiz_check_option_mobile" type="checkbox" id="40V" name="40V" value="40V" />
+              <label className="quiz_desc_option_mobile" htmlFor="40V">40V</label>
+            </div>
+            <br />
+            <div className="ui checkbox quiz_mobile">
+              <input className="quiz_check_option_mobile" type="checkbox" id="80V" name="80V" value="80V" />
+              <label className="quiz_desc_option_mobile" htmlFor="80V">80V</label>
+              <br />
+            </div>
+          </div>
+        )}
+
+        {Question3() && (
+          <div>
+            <div className="ui large header quiz_mobile">Quantas células são necessárias?</div>
+            <div className="ui checkbox quiz_mobile">
+              <input className="quiz_check_option_mobile" type="checkbox" id="6" name="6" value="6" />
+              <label className="quiz_desc_option_mobile" htmlFor="6">6</label>
+            </div>
+            <br />
+            <div className="ui checkbox quiz_mobile">
+              <input className="quiz_check_option_mobile" type="checkbox" id="8" name="8" value="8" />
+              <label className="quiz_desc_option_mobile" htmlFor="8">8</label>
+            </div>
+            <br />
+            <div className="ui checkbox quiz_mobile">
+              <input className="quiz_check_option_mobile" type="checkbox" id="10" name="10" value="10" />
+              <label className="quiz_desc_option_mobile" htmlFor="10">10</label></div>
+            <br />
+          </div>
+        )}
+
+        {Question4() && (
+          <div>
+            <div className="ui large header quiz_mobile">Qual o grau de proteção necessário?</div>
+            <div className="ui checkbox quiz_mobile">
+              <input className="quiz_check_option_mobile" type="checkbox" id="IP23" name="IP23" value="IP23" />
+              <label className="quiz_desc_option_mobile" htmlFor="IP23">IP23</label>
+            </div>
+            <br />
+            <div className="ui checkbox quiz_mobile">
+              <input className="quiz_check_option_mobile" type="checkbox" id="IP24(W)" name="IP24(W)" value="IP24(W)" />
+              <label className="quiz_desc_option_mobile" htmlFor="IP24(W)">IP24(W)</label>
+            </div>
+            <br />
+            <div className="ui checkbox quiz_mobile">
+              <input className="quiz_check_option_mobile" type="checkbox" id="IP55" name="IP55" value=" IP55" />
+              <label className="quiz_desc_option_mobile" htmlFor="IP55"> IP55</label>
+            </div>
+            <br />
+            <div className="ui checkbox quiz_mobile">
+              <input className="quiz_check_option_mobile" type="checkbox" id="IP56" name="IP56" value="IP56" />
+              <label className="quiz_desc_option_mobile" htmlFor="IP56">IP56</label>
+            </div>
+            <br />
+          </div>
+        )}
+
+        {Question5() && (
+          <div>
+            <div className="ui large header quiz_mobile">Qual a tensão necessária?</div>
+            <div className="ui checkbox quiz_mobile">
+              <input className="quiz_check_option_mobile" type="checkbox" id="220/380V" name="220/380V" value="220/380V" />
+              <label className="quiz_desc_option_mobile" htmlFor="220/380V">220/380V</label>
+            </div>
+            <br />
+            <div className="ui checkbox quiz_mobile">
+              <input className="quiz_check_option_mobile" type="checkbox" id="220/380/440V" name="220/380/440V" value="220/380/440V" />
+              <label className="quiz_desc_option_mobile" htmlFor="220/380/440V">220/380/440V</label>
+            </div>
+            <br />
+          </div>
+        )}
+
+        {progresso === 5 ? (
+          <div>
+            <div className="ui large center aligned header quiz_recomendation_mobile">Motor W50</div>
+            <img className="ui centered fluid image w50 quiz_recomendation_mobile" src={w50} />
+            <p className="description">Os motores W50 são projetados para aplicações industriais e garantem excelente performance e confiabilidade nas mais severas aplicações.</p>
+            <Link to='/product'>
+              <button className="ui huge fluid button details quiz_recomendation_mobile">Mais detalhes</button>
+            </Link>
+          </div>
+        ) : (
+          <div>
+            <button className="ui big button back quiz_btn_options_mobile" onClick={voltar}>Voltar</button>
+            <button className="ui big button next quiz_btn_options_mobile" onClick={proximo}>Próximo</button>
+          </div>
+        )}
+      </div>
+      <div className='box_quiz_title_similar_mobile'>
+        <FaStar color='var(--white)' size={40} />
+        <h1 className='quiz_title_similar_mobile' >Produtos Semelhantes</h1>
+      </div>
+      <ProductSmallCarousel />
+      <Footer />
+    </>
   )
 
   const getViewToRender = () => {
     if (screenSize.width > 900) {
-        return renderDesktopView();
-        // } else if (screenSize.width < 900 && screenSize.width > 500) {
-        //   return renderTabletView();
+      return renderDesktopView();
+      // } else if (screenSize.width < 900 && screenSize.width > 500) {
+      //   return renderTabletView();
     } else {
-        return renderMobileView();
+      return renderMobileView();
     }
-};
-return <>{getViewToRender()}</>;
+  };
+  return <>{getViewToRender()}</>;
 }
 
 export default Quiz
