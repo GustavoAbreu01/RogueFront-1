bd63b410074aa9865f1292d09fbfb1e75495011e
mary <105462946+mariazinhagameplys@users.noreply.github.com>
2023-08-21 15:10:57 -0300

    att

--- src/Pages/Save/Save.js
+++ src/Pages/Save/Save.js
@@ -14,94 +14,10 @@ import RecommendedSave from '../../Components/RecomandedProductsSave/Recommended
 import { BsFillBookmarkFill } from 'react-icons/bs'
 import { AiFillStar } from 'react-icons/ai'
 
-function Product() {
-  const [products, setProducts] = useState([]);
-
-  const produtoStatico = () => {
-    const produto = {
-      name: 'Motô',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec',
-      stockSize: 25,
-      price: 10.0,
-    };
-    return produto;
-  }
-
-  const adicionarProdutoAoLocalHost = () => {
-    const listaProdutos = JSON.parse(localStorage.getItem('products')) || [];
-    listaProdutos.push(produtoStatico());
-    localStorage.setItem('products', JSON.stringify(listaProdutos));
-    setProducts(listaProdutos); // Atualiza o estado com a nova lista de produtos
-    // let prod = produtoStatico()
-    // ProductService.create(prod);
-    console.log('produto adicionado');
-  };
-
-  const getLista = () => {
-    const lista = JSON.parse(localStorage.getItem('products'));
-    return lista || []; // Retorna uma lista vazia caso seja null
-  };
-
-  const lista = getLista();
-
-  const verify = () => {
-    const Registered = localStorage.getItem('verifyLogin');
-    if (Registered === "yes") {
-      return true
-    } else {
-      return false
-    }
-  }
-
-  const [screenSize, setScreenSize] = useState({ width: 0, height: 0 });
-
-    useEffect(() => {
-      function handleResize() {
-        setScreenSize({ width: window.innerWidth, height: window.innerHeight });
-      }
-      window.addEventListener('resize', handleResize);
-      handleResize();
-      return () => {
-        window.removeEventListener('resize', handleResize);
-      };
-    }, []);
-
-  if (lista.length === 0) {
-    return (
-      <>
-        {!verify() ? <Header /> : <HeaderLogin />}<WeggnerModal />
-        <div className='container_save_titles'>
-
-        </div>
-        <button onClick={() => adicionarProdutoAoLocalHost()}>Adicionar</button>
-
-      </>
-    );
-  } else {
-
-    return (
-      
-      <>
-        {!verify() ? <Header /> : <HeaderLogin />}<WeggnerModal />
-        <div className='container_save_titles'>
-        <div className='save_saved_product'>
-          <div className='box_title_similar_save'>
-            <BsFillBookmarkFill color='var(--white)' size={30} />
-            <h1 className='save_title'>Salvos</h1>
-          </div>
-            {products.map((item) => {
-              return <div key={item}><SaveCard item={item} /></div>;
-            })}
-        </div>
-        
-      </div>
-      </>
-    );
-  }
-}
 
 function Salvos() {
-
+  
+  
   const verify = () => {
     const Registered = localStorage.getItem('verifyLogin');
     if (Registered === "yes") {
@@ -110,14 +26,29 @@ function Salvos() {
       return false
     }
   }
+  const savedProducts = JSON.parse(localStorage.getItem('savedProducts')) || [];
 
   return (
-    <>
-      {/* {!verify() ? <Header /> : <HeaderLogin />}<WeggnerModal /> */}
-      <div>{Product()}</div>
-      {/* <Footer /> */}
-    </>
+      <>
+         {!verify() ? <Header /> : <HeaderLogin />}<WeggnerModal />
+          <WeggnerModal />
+          <div className='container_save_titles'>
+              <div className='save_saved_product'>
+                  <div className='box_title_similar_save'>
+                      <BsFillBookmarkFill color='var(--white)' size={30} />
+                      <h1 className='save_title'>Salvos</h1>
+                  </div>
+                  {savedProducts.map((item, index) => (
+                      <div key={index}>
+                          <SaveCard item={item} />
+                      </div>
+                  ))}
+              </div>
+          </div>
+          <Footer />
+      </>
   );
 }
+
 
 export default Salvos;
