498a69626dbfad2cf76b803375d9832499e93d4a
mary <105462946+mariazinhagameplys@users.noreply.github.com>
2023-08-21 15:28:29 -0300

    atatatatatatatatttatatttttt

--- src/Pages/Save/Save.js
+++ src/Pages/Save/Save.js
@@ -16,8 +16,8 @@ import { AiFillStar } from 'react-icons/ai'
 
 
 function Salvos() {
-  
-  
+
+
   const verify = () => {
     const Registered = localStorage.getItem('verifyLogin');
     if (Registered === "yes") {
@@ -28,25 +28,27 @@ function Salvos() {
   }
   const savedProducts = JSON.parse(localStorage.getItem('savedProducts')) || [];
 
+
   return (
-      <>
-         {!verify() ? <Header /> : <HeaderLogin />}<WeggnerModal />
-          <WeggnerModal />
-          <div className='container_save_titles'>
-              <div className='save_saved_product'>
-                  <div className='box_title_similar_save'>
-                      <BsFillBookmarkFill color='var(--white)' size={30} />
-                      <h1 className='save_title'>Salvos</h1>
-                  </div>
-                  {savedProducts.map((item, index) => (
-                      <div key={index}>
-                          <SaveCard item={item} />
-                      </div>
-                  ))}
-              </div>
+    <>
+      {!verify() ? <Header /> : <HeaderLogin />}<WeggnerModal />
+      <WeggnerModal />
+      <div className='container_save_titles'>
+        <div className='save_saved_product'>
+          <div className='box_title_similar_save'>
+            <BsFillBookmarkFill color='var(--white)' size={30} />
+            <h1 className='save_title'>Salvos</h1>
           </div>
-          <Footer />
-      </>
+          {savedProducts.map((item, index) => (
+            <div key={index}>
+              <SaveCard item={item} />
+            </div>
+          ))}
+
+        </div>
+      </div>
+      <Footer />
+    </>
   );
 }
 
