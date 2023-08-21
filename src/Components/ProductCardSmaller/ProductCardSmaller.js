427c3449276f17044961189a36aa06d39646d300
mary <105462946+mariazinhagameplys@users.noreply.github.com>
2023-08-21 20:20:19 -0300

    att

--- src/Components/ProductCardSmaller/ProductCardSmaller.js
+++ src/Components/ProductCardSmaller/ProductCardSmaller.js
@@ -70,33 +70,121 @@ function SmallProductCard(product) {
         }
         )
     }
-
-
     const AddProductInCompare = () => {
-        Swal.fire({
-            title: 'Produto adicionado a comparação!',
-            icon: 'success',
-            showConfirmButton: true,
-            confirmButtonText: 'Ir para a tela comparação',
-            confirmButtonColor: 'var(--blue-primary)',
-            position: 'top-end',
-            timer: 5000,
-            timerProgressBar: true,
-            toast: true,
-            width: 400,
-            showClass: {
-                popup: 'animate__animated animate__backInRight'
-            },
-            hideClass: {
-                popup: 'animate__animated animate__backOutRight'
-            },
-        }).then((result) => {
-            if (result.isConfirmed) {
-                window.location.href = "/compare"
-            }
+        const productsInCompare = JSON.parse(localStorage.getItem('productsInCompare')) || [];
+    
+        if (productsInCompare.length <= 2) {
+            productsInCompare.push(product);
+            localStorage.setItem('productsInCompare', JSON.stringify(productsInCompare));
+    
+            Swal.fire({
+                title: 'Produto adicionado a comparação!',
+                icon: 'success',
+                showConfirmButton: true,
+                confirmButtonText: 'Ir para a tela comparação',
+                confirmButtonColor: 'var(--blue-primary)',
+                position: 'top-end',
+                timer: 5000,
+                timerProgressBar: true,
+                toast: true,
+                width: 400,
+                showClass: {
+                    popup: 'animate__animated animate__backInRight'
+                },
+                hideClass: {
+                    popup: 'animate__animated animate__backOutRight'
+                },
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    window.location.href = "/compare";
+                }
+            });
+        } else {
+            Swal.fire({
+                title: 'Produto não pode ser adicionado no carrinho',
+                icon: 'warning',
+                showConfirmButton: true,
+                confirmButtonText: 'Ir para a tela comparação',
+                confirmButtonColor: 'var(--blue-primary)',
+                position: 'top-end',
+                timer: 5000,
+                timerProgressBar: true,
+                toast: true,
+                width: 400,
+                showClass: {
+                    popup: 'animate__animated animate__backInRight'
+                },
+                hideClass: {
+                    popup: 'animate__animated animate__backOutRight'
+                },
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    window.location.href = "/compare";
+                }
+            });
         }
-        )
+        
     }
+
+    
+    const AddProductInCompareTablet = () => {
+        const productsInCompare = JSON.parse(localStorage.getItem('productsInCompare')) || [];
+    
+        if (productsInCompare.length <= 1) {
+            productsInCompare.push(product);
+            localStorage.setItem('productsInCompare', JSON.stringify(productsInCompare));
+    
+            Swal.fire({
+                title: 'Produto adicionado a comparação!',
+                icon: 'success',
+                showConfirmButton: true,
+                confirmButtonText: 'Ir para a tela comparação',
+                confirmButtonColor: 'var(--blue-primary)',
+                position: 'top-end',
+                timer: 5000,
+                timerProgressBar: true,
+                toast: true,
+                width: 400,
+                showClass: {
+                    popup: 'animate__animated animate__backInRight'
+                },
+                hideClass: {
+                    popup: 'animate__animated animate__backOutRight'
+                },
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    window.location.href = "/compare";
+                }
+            });
+        } else {
+            Swal.fire({
+                title: 'Produto não pode ser adicionado no carrinho',
+                icon: 'warning',
+                showConfirmButton: true,
+                confirmButtonText: 'Ir para a tela comparação',
+                confirmButtonColor: 'var(--blue-primary)',
+                position: 'top-end',
+                timer: 5000,
+                timerProgressBar: true,
+                toast: true,
+                width: 400,
+                showClass: {
+                    popup: 'animate__animated animate__backInRight'
+                },
+                hideClass: {
+                    popup: 'animate__animated animate__backOutRight'
+                },
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    window.location.href = "/compare";
+                }
+            });
+        }
+        
+    }
+
+
+
 
 
     const [screenSize, setScreenSize] = useState({ width: 0, height: 0 });
@@ -117,7 +205,43 @@ function SmallProductCard(product) {
         <div className='container_product_card_smaller'>
             <div className='container_product_card_smaller_action_icon'>
                 <div className='product_card_small_button_compare'>
-                    <button onClick={AddProductInCompare} className="ui icon button product_card_smaller_compare">
+                    <button onClick={() => AddProductInCompare(product)} className="ui icon button product_card_smaller_compare">
+                        <i className="exchange alternate icon"></i>
+                    </button>
+                </div>
+                <button onClick={() => AddProductInSave(product)} className="ui icon button product_card_smaller">
+                    <i className="bookmark icon "></i>
+                </button>
+                <button onClick={() => AddProductInCart(product)} className="ui icon button product_card_smaller">
+                    <i className="cart plus icon"></i>
+                </button>
+            </div>
+            <Link to='/product'>
+                <div className='box_product_card_smaller_image'>
+                    <img src={motor} alt='' className='product_card_smaller_image' height={110} width={110}></img>
+                </div>
+                <div >
+                    <div className='box_product_card_smaller_title'>
+                        <h3 className='product_card_smaller_title'>W12 Motor</h3>
+                    </div>
+                </div>
+                <div className='box_product_card_smaller_price'>
+                    <h3 className='product_card_smaller_price'>R$ 1.259,00</h3>
+                    <p className='product_card_smaller_price_info'>Á vista no pix</p>
+                </div>
+                <Link to='/cart'>
+                    <div className='box_product_card_smaller_button'>
+                        <button className="fluid ui button product_card_smaller">Comprar</button>
+                    </div>
+                </Link>
+            </Link >
+        </div>
+    )
+    const renderTabletView = () => (
+        <div className='container_product_card_smaller'>
+            <div className='container_product_card_smaller_action_icon'>
+                <div className='product_card_small_button_compare'>
+                    <button onClick={() => AddProductInCompareTablet(product)} className="ui icon button product_card_smaller_compare">
                         <i className="exchange alternate icon"></i>
                     </button>
                 </div>
@@ -188,8 +312,8 @@ function SmallProductCard(product) {
     const getViewToRender = () => {
         if (screenSize.width > 900) {
             return renderDesktopView();
-            // } else if (screenSize.width < 900 && screenSize.width > 500) {
-            //   return renderTabletView();
+            } else if (screenSize.width < 900 && screenSize.width > 500) {
+              return renderTabletView();
         } else {
             return renderMobileView();
         }
