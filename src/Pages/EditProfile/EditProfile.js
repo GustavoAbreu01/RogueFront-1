17ef508541f4affa6a54508cd8b12edaf6f8651e
Gustavo Guilherme de Abreu <110638920+Fennek01@users.noreply.github.com>
2023-08-22 14:12:32 -0300

    ATT

--- src/Pages/EditProfile/EditProfile.js
+++ src/Pages/EditProfile/EditProfile.js
@@ -28,6 +28,21 @@ function EditProfile() {
 
   const [activeSection, setActiveSection] = useState(1);
   const [editMode, setEditMode] = useState(false);
+  const [showComponent, setShowComponent] = useState(false);
+
+  useEffect(() => {
+    const storedValue = localStorage.getItem('showComponent');
+    if (storedValue) {
+      setShowComponent(storedValue === 'true');
+    }
+  }, []);
+
+  const handleCheckboxChange = (event) => {
+    const newValue = event.target.checked;
+    setShowComponent(newValue);
+    // Store checkbox value in localStorage
+    localStorage.setItem('showComponent', newValue);
+  };
 
   const changeInfo = (event) => {
     event.preventDefault();
@@ -715,7 +730,9 @@ function EditProfile() {
                     </div>
                     <div className="acessebility_profile_switcher">
                       <div className="checkbox-wrapper-6">
-                        <input className="tgl tgl-light" id="cb1-1" type="checkbox" />
+                        <input className="tgl tgl-light" id="cb1-1" type="checkbox"
+                        onChange={handleCheckboxChange}
+                        checked={showComponent} />
                         <label className="tgl-btn" for="cb1-1" />
                       </div>
                     </div>
